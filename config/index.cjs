76f34704fe6f78151f754c04821bd0ab5dd88b52
boxofclu <48398050+boxofclu@users.noreply.github.com>
2022-12-01 16:18:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小张',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oOvxQ556PLzJy9QCtpuqH8t-eOYI',
+      id: 'oOvxQ51DQvTJqE2-NhszXhjuIdXw',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'r0PJIlXlwsNYPGHyQvT7b6zbrIOAsl-_09bDyZ252ds',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
