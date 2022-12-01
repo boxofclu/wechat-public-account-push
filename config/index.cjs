a5ee4720a674e46292798bbbe146fab11dd949f5
boxofclu <48398050+boxofclu@users.noreply.github.com>
2022-12-01 16:06:57 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oOvxQ556PLzJy9QCtpuqH8t-eOYI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'UOGApZ4yUM1ILTd9MTnmUsOQfvJ4S8kb8mBr56UpuQE',
+      useTemplateId: 'iznHlPnHeepTbSr56leng5t-DHUbGBqGllEoSA8dnJY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-03',
       festivals: [
